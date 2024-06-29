490fe640add518355d5466623e0c7646724bdd59
David Opoku <david@cyboticx.com>
2024-06-29 12:12:30 +0000

    fix #8 for web build on vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@prism/types": path.resolve(__dirname, "../../packages/types/src"),
     },
   },
 });
