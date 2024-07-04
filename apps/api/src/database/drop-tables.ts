e6884a142959e39ef2ae979992aeef1119a727f4
David Opoku <david@cyboticx.com>
2024-07-04 15:20:55 +0000

    fix for ci

--- apps/api/src/database/drop-tables.ts
+++ apps/api/src/database/drop-tables.ts
@@ -1,33 +1,36 @@
-import { config } from 'dotenv';
-import postgres from 'postgres';
+import { config } from "dotenv";
+import postgres from "postgres";
 
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { sql } from 'drizzle-orm';
+import { drizzle } from "drizzle-orm/postgres-js";
+import { sql } from "drizzle-orm";
 
-config({ path: '.dev.vars' });
+config({ path: ".dev.vars" });
 
 const url = `${process.env.DATABASE_URL}?options=project%3D${process.env.PROJECT_NAME}`;
-const db = drizzle(postgres(url, { ssl: 'require', max: 1 }));
+const db = drizzle(postgres(url, { ssl: "require", max: 1 }));
 const main = async () => {
-	try {
-		await db.execute(sql`BEGIN`);
+  try {
+    await db.execute(sql`BEGIN`);
 
-		const tables = await db.execute(sql`
+    const tables = await db.execute(sql`
       SELECT tablename
       FROM pg_tables
       WHERE schemaname = 'public';
     `);
 
-		tables.forEach(async (table) => {
-			const tablename = table.tablename as string;
-			await db.execute(sql`DROP TABLE IF EXISTS ${sql.identifier(tablename)} CASCADE`);
-		});
+    tables.forEach(async (table) => {
+      const tablename = table.tablename as string;
+      await db.execute(
+        sql`DROP TABLE IF EXISTS ${sql.identifier(tablename)} CASCADE`,
+      );
+    });
 
-		await db.execute(sql`COMMIT`);
-		process.exit(0);
-	} catch (error) {
-		console.error(error);
-		process.exit(1);
-	}
+    await db.execute(sql`COMMIT`);
+    console.log("Database tables dropped.");
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 main();
